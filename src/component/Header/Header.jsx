a3c4c9e555e4ae174e0f80e653f63ae2838b7533
rabby6530 <you@example.com>
2023-04-10 09:26:25 +0600

    navbar

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
         <nav>
             <h1>Job Opportunity </h1>
-            <Link to="/" >Home</Link>
-            <Link to="/statistic" >Statistics</Link>
-            <Link to="/job" >Applied Jobs</Link>
-            <Link to="/blog" >Blog</Link>
-            <button>Start Applying</button>
+            <div>
+                <Link to="/" >Home</Link>
+                <Link to="/statistic" >Statistics</Link>
+                <Link to="/job" >Applied Jobs</Link>
+                <Link to="/blog" >Blog</Link>
+            </div>
+            <button >Start Applying</button>
         </nav>
     );
 };
